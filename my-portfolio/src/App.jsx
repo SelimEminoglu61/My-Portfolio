5b5bcee39396e760dc74e2f1900ae0f555195277
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-09-21 12:56:05 +0300

    İlk Kurulum Yapıldı
    Dosya yüklemeleri yapıldı.

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from "./pages/Page";
+import "./assets/css/style.css";
+
+function App() {
+  return (
+    <>
+      <Page />
+    </>
+  );
+}
+
+export default App;
---
+++ my-portfolio/src/pages/Page.jsx
@@ -0,0 +1,7 @@
+import "./stylePage.css";
+
+function Page() {
+  return <div className="background">Page</div>;
+}
+
+export default Page;
