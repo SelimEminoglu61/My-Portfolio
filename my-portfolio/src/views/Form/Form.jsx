7b143a4bc17864b20e3d9d0f8d42caba3c5ed2b8
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-10-02 14:27:49 +0300

    Form Güncellemesi
    Form kısmı için eklemeler yapıldı, düzenlemeler ve fonksiyon eklenecek.

--- my-portfolio/src/views/Form/Form.jsx
+++ my-portfolio/src/views/Form/Form.jsx
@@ -3,8 +3,33 @@ import "./styleForm.css";
 function Form() {
   return (
     <form>
-      <label for="name">Your Name:</label>
-      <input name="name" type="text" />
+      <h3>Send Mail To Me</h3>
+      <div>
+        <label htmlFor="name">Your Name:</label>
+        <input name="name" className="nameInput" type="text" autoFocus />
+      </div>
+      <div>
+        <label htmlFor="surname">Your Surname:</label>
+        <input name="surname" className="nameInput" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Your Email:</label>
+        <input name="email" className="nameInput" type="email" />
+      </div>
+      <div className="messageDiv">
+        <label htmlFor="message">Your Message:</label>
+        <textarea
+          id="message"
+          className="nameInput"
+          rows="10"
+          columns="100"
+          required
+          maxLength="500"
+          placeholder="Limit to 500 characters"
+          wrap="soft"
+        />
+      </div>
+      <input className="submitBtn" value="Send Mail" type="submit" />
     </form>
   );
 }
