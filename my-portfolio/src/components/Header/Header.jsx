53dd862942c31214f8dfbecc0ab3c5e33fd58ac8
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-10-11 12:05:09 +0300

    Header Ve Title Güncellemesi
    Responsive düzenlemeleri yapıldı, hamburger menu eklendi.

--- my-portfolio/src/components/Header/Header.jsx
+++ my-portfolio/src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-scroll";
 import "./styleHeader.css";
+import { useState } from "react";
 
 function Header() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  function isOpenHamMenu() {
+    if (isOpenMenu == false) {
+      setIsOpenMenu(true);
+    } else {
+      setIsOpenMenu(false);
+    }
+  }
+
   return (
     <div className="headerDiv">
       <div>
@@ -35,6 +46,51 @@ function Header() {
           </Link>
         </ul>
       </div>
+      <div className="hamMenu" onClick={() => isOpenHamMenu()}>
+        <img src="./src/assets/icons/menu.png" alt="icon" />
+      </div>
+      {isOpenMenu && (
+        <div className="burgerList">
+          <ul>
+            <Link
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-70}
+            >
+              <li>Home</li>
+            </Link>
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-90}
+            >
+              <li>About</li>
+            </Link>
+            <Link
+              to="experience"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-90}
+            >
+              <li>Experience</li>
+            </Link>
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-90}
+            >
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
