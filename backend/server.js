7b703ec8e6a2173b5ee5c8070c99c64b989f5780
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-10-16 12:15:11 +0300

    Backend Güncellemesi
    İlk email gönderme çalışması yapıldı.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,34 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-// E-posta gönderme işlemi için gerekli kodlar burada olmalı
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "1967selim61@gmail.com",
+    pass: "*******",
+  },
+});
+
+transporter.verify(function (error, success) {
+  if (error) throw error;
+
+  console.log("bağlantı sağlandı", success);
+});
+
+let bilgiler = {
+  from: "Selim Eminoğlu 1967selim61@gmail.com",
+  to: "selim.eminoglu.9257@gmail.com",
+  subject: "Portfolyo Mail Service",
+  text: "merhaba abi",
+};
+
+transporter.sendMail(bilgiler, function (error, info) {
+  if (error) throw error;
+
+  console.log("başarılı", info.response);
+});
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda çalışıyor.`);
