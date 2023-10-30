20e2c01c3002695fc5ace84bc347224665f000fe
Selim Eminoğlu <56717476+SelimEminoglu61@users.noreply.github.com>
2023-10-30 19:42:54 +0300

    Yayın Hazırlıgı
    Nokta kaldırıldı.

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,10 @@ const nodemailer = require("nodemailer");
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "./my-portfolio/dist")));
+app.use(express.static(path.join(__dirname, "/my-portfolio/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./my-portfolio/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/my-portfolio/dist/index.html"));
 });
 
 let transporter = nodemailer.createTransport({
