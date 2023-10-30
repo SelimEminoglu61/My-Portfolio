f455133f74cfb32bdc628dc5adb423b38956b021
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-10-30 13:50:09 +0300

    Yayına Hazırlama

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const nodemailer = require("nodemailer");
 app.use(cors());
 app.use(express.json());
 
-app.use("/", express.static(path.join(__dirname, "../my-portfolio/dist")));
+app.use(express.static(path.join(__dirname, "../my-portfolio/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../my-portfolio/dist/index.html"));
