b98c4a0310818a330144a66117b9e095a74c10e9
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-11-03 10:06:53 +0300

    Backend Güncellemesi

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const morgan = require("morgan");
 
 const nodemailer = require("nodemailer");
 
