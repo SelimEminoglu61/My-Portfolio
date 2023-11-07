2772c87b2b34d2aea76691b207a11e8284fab8b8
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-11-07 15:11:26 +0300

    Backend Güncellemesi

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://selim-eminoglu-portfolio.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
   })
 );
 
