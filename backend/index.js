0884939c439e231b0b2258b039fcade7ed8b9d5c
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-11-07 11:33:32 +0300

    Cors Güncellemesi

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,43 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "https://selim-eminoglu-portfolio.vercel.app",
+  })
+);
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://selim-eminoglu-portfolio.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token,Authorization, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  return await fn(req, res);
-};
+// const allowCors = (fn) => async (req, res) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://selim-eminoglu-portfolio.vercel.app"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-CSRF-Token,Authorization, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   return await fn(req, res);
+// };
 
-app.options("/", (req, res) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://selim-eminoglu-portfolio.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.status(200).end();
-});
+// app.options("/", (req, res) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://selim-eminoglu-portfolio.vercel.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+//   res.status(200).end();
+// });
 
-app.use(allowCors);
+// app.use(allowCors);
 
 let transporter = nodemailer.createTransport({
   service: "Gmail",
