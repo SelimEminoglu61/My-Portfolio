9094b330b7ef8808be7a903afb2b901b19fb27c1
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-11-08 11:51:33 +0300

    Backend Güncellemesi

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,17 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 app.use(express.json());
+
+const allowedOrigins = ["https://selim-eminoglu-portfolio.vercel.app"];
 app.use(
   cors({
-    origin: "https://selim-eminoglu-portfolio.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
