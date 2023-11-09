fa0e3abd6f07e7a028af29e9b2d04c4667084154
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-11-09 14:54:28 +0300

    Backend Güncellemesi

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,15 @@ const path = require("path");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://selim-eminoglu-portfolio.vercel.app",
-    methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
-    allowedHeaders: "Content-Type,Authorization",
+    origin: ["https://selim-eminoglu-portfolio.vercel.app"],
+    methods: ["GET,POST,PUT,DELETE,PATCH,OPTIONS"],
+    allowedHeaders: ["Content-Type,Authorization"],
     credentials: true,
   })
 );
 
-app.use(express.static(path.join(__dirname, "../my-portfolio/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../my-portfolio/dist/index.html"));
+app.get("/", (req, res) => {
+  res.json("Hello");
 });
 
 let transporter = nodemailer.createTransport({
