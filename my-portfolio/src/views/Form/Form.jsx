2eeabf3b0b9dfd7734b893dc7184f659f92183c4
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-11-10 14:53:04 +0300

    Front-end Güncellemesi

--- my-portfolio/src/views/Form/Form.jsx
+++ my-portfolio/src/views/Form/Form.jsx
@@ -65,12 +65,11 @@ async function postMailInfo(url, data) {
 }
 
 const onSubmit = async (values, actions) => {
-  postMailInfo(
-    "https://portfolio-backend-selim-eminoglus-projects.vercel.app/",
-    values
-  ).then((response) => {
-    successInfoToast(response.name, response.surname);
-  });
+  postMailInfo("https://my-portfolio-backend-three.vercel.app/", values).then(
+    (response) => {
+      successInfoToast(response.name, response.surname);
+    }
+  );
 
   await new Promise((resolve) => {
     setTimeout(resolve, 1000);
