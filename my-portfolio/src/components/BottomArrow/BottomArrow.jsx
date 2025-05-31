c8f2b5c94c5bef2211fc34534a519f23e44b82a8
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2025-05-31 11:52:01 +0300

    Bg Ve İkon Güncellemesi
    background değiştirildi, bazı ikonlar güncelledi.

--- my-portfolio/src/components/BottomArrow/BottomArrow.jsx
+++ my-portfolio/src/components/BottomArrow/BottomArrow.jsx
@@ -1,11 +1,18 @@
 import "./styleBottomArrow.css";
 import "../../assets/css/style.css";
+import downİcon from "../../assets/icons/icon-down.png";
 
 function BottomArrow() {
   return (
     <div className="container">
       <div className="arrowDiv">
-        <img className="arrowDown" src="/icons/down-arrow.png" alt="icon" />
+        <img
+          className="arrowDown"
+          src={downİcon}
+          alt="icon"
+          width={100}
+          height={100}
+        />
       </div>
     </div>
   );
--- my-portfolio/src/components/TitleSection/TitleSect.jsx
+++ my-portfolio/src/components/TitleSection/TitleSect.jsx
@@ -5,10 +5,12 @@ function TitleSect() {
     <div className="backgroundTitle" id="home">
       <div className="titleDiv">
         <div className="textDiv">
-          <h2 className="titleBlue">Hi,I'm</h2>
-          <h2 className="titleWhite">Selim</h2>
-          <h2 className="titleBlue">Eminoğlu</h2>
-          <p className="titleJob">Front End Developer</p>
+          <div className="borderText">
+            <h2 className="titleBlue">Hi,I'm</h2>
+            <h2 className="titleWhite">Selim</h2>
+            <h2 className="titleBlue">Eminoğlu</h2>
+            <p className="titleJob">Front End Developer</p>
+          </div>
         </div>
         <div className="imgDiv"></div>
       </div>
