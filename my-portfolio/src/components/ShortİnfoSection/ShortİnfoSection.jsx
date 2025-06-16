f290b019dd01e8c690baf0117c8afc05a34a6a4a
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2025-06-16 22:23:25 +0300

    ShortİnfoSection Güncellemesi
    Yeni yapı eklenmeye başlandı.

--- my-portfolio/src/components/ShortİnfoSection/ShortİnfoSection.jsx
+++ my-portfolio/src/components/ShortİnfoSection/ShortİnfoSection.jsx
@@ -1,7 +1,22 @@
 import "./styleShortİnfo.css";
+import "../../assets/css/style.css";
+import { useState } from "react";
 
 function ShortİnfoSection() {
-  return <div className="backgrDiv">ShortİnfoSection</div>;
+  const [myProjects, setMyProjects] = useState([]);
+
+  fetch("https://api.github.com/repos/SelimEminoglu61")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  return (
+    <div className="backgrDiv">
+      <div className="midTitleDiv">
+        <h3>My Projects</h3>
+      </div>
+      <div className="myProjectsDiv">projeler</div>
+    </div>
+  );
 }
 
 export default ShortİnfoSection;
