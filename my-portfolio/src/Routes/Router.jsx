efbef0a9fec876ccba4aa30db265e51253d83ac2
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2025-07-10 10:51:16 +0300

    Performans Güncellemesi
    Sayfalar lazy ile güncellendi.

--- my-portfolio/src/Routes/Router.jsx
+++ my-portfolio/src/Routes/Router.jsx
@@ -1,16 +1,41 @@
 import { useRoutes } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
 
 import MainPage from "../pages/MainPage";
-import AboutPage from "../pages/AboutPage";
-import ExpPage from "../pages/ExpPage";
-import ContactPage from "../pages/ContactPage";
+
+const AboutPage = lazy(() => import("../pages/AboutPage"));
+const ExpPage = lazy(() => import("../pages/ExpPage"));
+const ContactPage = lazy(() => import("../pages/ContactPage"));
+
+import "../assets/css/style.css";
 
 const Router = () => {
   const routes = useRoutes([
     { path: "/", element: <MainPage /> },
-    { path: "/about", element: <AboutPage /> },
-    { path: "/contact", element: <ContactPage /> },
-    { path: "/experience", element: <ExpPage /> },
+    {
+      path: "/about",
+      element: (
+        <Suspense classname="fallback">
+          <AboutPage />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/contact",
+      element: (
+        <Suspense classname="fallback">
+          <ContactPage />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/experience",
+      element: (
+        <Suspense classname="fallback">
+          <ExpPage />
+        </Suspense>
+      ),
+    },
   ]);
   return routes;
 };
